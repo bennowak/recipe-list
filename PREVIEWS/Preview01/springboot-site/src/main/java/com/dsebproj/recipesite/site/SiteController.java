bcb2860f58822da3284af3b729f52fc37e9f1725
Ben Nowak <bennnowak@gmail.com>
2018-12-18 21:50:03 -0500

    Fixed index.html template and SiteController.java issue.

--- PREVIEWS/Preview01/springboot-site/src/main/java/com/dsebproj/recipesite/site/SiteController.java
+++ PREVIEWS/Preview01/springboot-site/src/main/java/com/dsebproj/recipesite/site/SiteController.java
@@ -1,5 +1,8 @@
 package com.dsebproj.recipesite.site;
 
+import com.dsebproj.recipesite.recipe.Recipe;
+import com.dsebproj.recipesite.recipe.RecipeRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,9 @@ import java.util.List;
 @Controller
 public class SiteController {
 
+    @Autowired
+    private RecipeRepository recipeRepo;
+
 //    @Autowired
 //    private SiteRepository siteRepo;
 //
@@ -18,10 +24,14 @@ public class SiteController {
 
 //    This route handles the index page
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index(Recipe recipe) {
         ModelAndView mv = new ModelAndView("index");
         mv.addObject("pageTitle", "Index");
+        mv.addObject("allRecipes", recipeRepo.findAll());
+//        System.out.println(recipeList);
         return mv;
+
+
     }
 
 //    This route handles the about page
