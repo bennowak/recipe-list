b5f7891b5746a9c9816c0f5c5e000119df9bfd2b
symien moore <symien.moore@gmail.com>
2018-12-20 08:19:21 -0500

    landing and about info

--- recipe-list-sts/src/main/java/com/techtalentsouth/RecipeList/recipe/RecipeController.java
+++ recipe-list-sts/src/main/java/com/techtalentsouth/RecipeList/recipe/RecipeController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.techtalentsouth.TechTalentBlog.BlogPost;
+
 
 @Controller
 public class RecipeController {
