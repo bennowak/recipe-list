f9b2a68fe56e25e06a90ed88ec788c216b9416ce
Ben Nowak <bennnowak@gmail.com>
2018-12-21 01:31:43 -0500

    General cleanup. Moved springboot-site folder to the repository root, and deleted extranious files.

--- recipe-list-sts/src/main/java/com/techtalentsouth/RecipeList/recipe/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techtalentsouth.RecipeList.recipe;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-
-}
